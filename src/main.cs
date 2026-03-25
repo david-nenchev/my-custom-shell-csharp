cbdb821438f39da4b09db8179614918ed72e2ec4
Deyvid Nenchev <deyvidnenchev@gmail.com>
2026-03-25 19:15:01 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,59 +2,39 @@ class Program
 {
     static void Main()
     {
-        var shellBuiltIn = "is a shell builtin";
         while (true)
         {
             Console.Write("$ ");
 
             var commandQuene = new Queue<string>(Console.ReadLine().Split([" "], StringSplitOptions.RemoveEmptyEntries));
+            string[] shellCommands = { "exit", "echo", "type" };
             //var command = parsedText.FirstOrDefault();
             //var arguments = string.Join(" ", parsedText[1..]);
-            string prevCommand = null;
 
+            
 
-            while (commandQuene.Count > 0)
+            while(commandQuene.Count > 0)
             {
-               
                 var command = commandQuene.Dequeue();
 
                 switch (command)
                 {
                     case "exit":
-                        if (prevCommand == "type")
-                        {
-                            Console.WriteLine($"{command} {shellBuiltIn}");
-                            prevCommand = null;
-                            break;
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        return;
                     case "echo":
-                        if (prevCommand == "type")
-                        {
-                            Console.WriteLine($"{command} {shellBuiltIn}");
-                            prevCommand = null;
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine(string.Join(" ", commandQuene));
-                            break;
-                        }
+                        Console.WriteLine(string.Join(" ", commandQuene));
+                        break;
                     case "type":
-                        if (prevCommand == "type") 
+                        var argument = string.Join(" ", commandQuene);
+                        if (shellCommands.Contains(argument))
                         {
-                            Console.WriteLine($"{command} {shellBuiltIn}");
-                            prevCommand = null;
-                            break;
+                            Console.WriteLine($"{argument} is a shell builtin");
                         }
                         else
                         {
-                            prevCommand = "type";
-                            break;
+                            Console.WriteLine($"{argument}: not found");
                         }
+                        break;
                     default:
                         Console.WriteLine($"{command}: command not found");
                         break;
