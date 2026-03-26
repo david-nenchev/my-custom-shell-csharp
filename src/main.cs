c64f3248220c3c9ed7189f638f8cf6816b13babc
Deyvid Nenchev <deyvidnenchev@gmail.com>
2026-03-26 09:47:20 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 class Program
 {
     static void Main()
@@ -8,37 +10,79 @@ class Program
 
             var commandQuene = new Queue<string>(Console.ReadLine().Split([" "], StringSplitOptions.RemoveEmptyEntries));
             string[] shellCommands = { "exit", "echo", "type" };
-            
 
-           
-                var command = commandQuene.Dequeue();
-                var argument = string.Join(" ", commandQuene);
+
+
+            var command = commandQuene.Dequeue();
+            var argument = string.Join(" ", commandQuene);
 
             switch (command)
-                {
-                    case "exit":
-                        return;
-                    case "echo":
-                        Console.WriteLine(string.Join(" ", commandQuene));
-                        break;
-                    case "type":
-                        if (shellCommands.Contains(argument))
-                        {
-                            Console.WriteLine($"{argument} is a shell builtin");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"{argument}: not found");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine($"{command}: command not found");
-                        break;
-                }
-            
+            {
+                case "exit":
+                    return;
+                case "echo":
+                    Console.WriteLine(string.Join(" ", commandQuene));
+                    break;
+                case "type":
+                    if (shellCommands.Contains(argument))
+                    {
+                        Console.WriteLine($"{argument} is a shell builtin");
+                    }
+                    else
+                    {
+                        string root = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+                        var fileMatches = Directory.EnumerateFiles(root, argument + ".*", SearchOption.AllDirectories);
 
-         
+                        foreach(var fileMatch in fileMatches)
+                        {
+                            if (!string.IsNullOrEmpty(fileMatch))
+                            {
+                                var canExecute = CanExecute(fileMatch);
+
+                                if (canExecute)
+                                {
+                                    Console.WriteLine($"{argument} is {fileMatch}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{argument}: not found");
+                            }
+                        }
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"{command}: command not found");
+                    break;
+            }
         }
 
+    }
+
+    static bool CanExecute(string path)
+    {
+        try
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    UseShellExecute = false
+                }
+            };
+
+            process.Start();
+            process.Kill(); // stop immediately
+            return true;
+        }
+        catch (System.ComponentModel.Win32Exception)
+        {
+            return false; // no permission or not executable
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
