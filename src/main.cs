e7e2245a4f740934927d40ee4ee5c995bed7a17e
Deyvid Nenchev <deyvidnenchev@gmail.com>
2026-03-26 10:11:52 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -36,7 +36,7 @@ class Program
                         foreach(var path in paths)
                         {
                             var fullPath = Path.Combine(root, path);
-                            Console.WriteLine(root);
+                            Console.WriteLine(fullPath);
                             var fileExists = File.Exists(path);
                             if (fileExists)
                             {
