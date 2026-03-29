997d299dba9b30ea3e5dced55c354e9580c0727c
Deyvid Nenchev <deyvidnenchev@gmail.com>
2026-03-29 19:26:22 +0300

    codecrafters submit [skip ci]

--- src/parsedInputModel.cs
+++ src/parsedInputModel.cs
@@ -4,13 +4,13 @@ namespace codecrafters.models
 {
     public class ParsedInputModel
     {
-        public ParsedInputModel(string parsedInput, string? redirectLocation  = null, string? op = null)
+        public ParsedInputModel(string[] parsedInput, string? redirectLocation  = null, string? op = null)
         {
                 this.ParsedInput = parsedInput;
                 this.RedirectLocation = redirectLocation;
                 this.Operator = op;
         }
-        public string ParsedInput { get; set; }
+        public string[] ParsedInput { get; set; }
 
         public string? RedirectLocation { get; set; }
 
